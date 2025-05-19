e9afd61f004362ab84236360835636d3157fbe10
Pitu <pitu.ebensperger@gmail.com>
2025-05-18 21:26:25 -0400

    404 illustration

--- src/components/notfound/NotFound.jsx
+++ src/components/notfound/NotFound.jsx
@@ -8,11 +8,12 @@ const NotFound = () => {
   return (
     <div className="error-page">
       <Link to='/home' className="btn-back"><FontAwesomeIcon icon={faArrowLeft} /></Link>
-      <h1 className="page-title">Error 404</h1>
+
       <div className="error-container">
+              <div className="error-img"></div>
         <h2>¡Ups! Página no encontrada</h2>
         <p>Lo sentimos, la página que estás buscando no existe.</p>
-        <Link to='/home' className="btn-home">Volver a la página de inicio</Link>
+        <Link to='/' className="btn-home">Volver a la página de inicio</Link>
       </div>
     </div>
   );
